992f727b1db7608157105fba6d00913933b60f63
PrasannahRam <rprasu2005@gmail.com>
2025-08-03 23:57:30 +0530

    data.json

--- src/component/Results_page.js
+++ src/component/Results_page.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import Results from './Results'
 import './Result.css'
+import data from './data.json' 
 
 function Results_page() {
-  const [year, setyear] = useState(2023)
-  const [exam, setexam] = useState('O/L')
+  const [year, setyear] = useState()
+  const [exam, setexam] = useState()
   const [indexno, setindexno] = useState('')
   const [nic, setnic] = useState()
   const [idtype, setidtype] = useState('indexno')
@@ -42,13 +43,22 @@ function Results_page() {
     <div>
       <div>
 
-        <select  value={exam} onChange={(e) => setexam(e.target.value)} style={{ width: 200, padding: '10px', margin: '10px', borderRadius: '5px' }}>
-          <option value="O/L">O/L</option>
-          <option value="A/L">A/L</option>
+        <select  value={exam} onChange={(e) => setexam(e.target.value)} style={{ width: '20%', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+          <option defaultChecked>Select Exam</option>
+          {Object.keys(data).map((e)=>{
+            return(
+              <option value={e} key={e}>{e}</option>
+            )
+            
+          })}
         </select>
-        <select value={year} onChange={(e) => setyear(e.target.value)} style={{ width: 300, padding: '10px', margin: '10px', borderRadius: '5px' }}>
-          <option value='2023'>2023</option>
-          <option value="2024">2024</option>
+        <select value={year} onChange={(e) => setyear(e.target.value)} style={{ width: '20% ', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+          <option defaultChecked>Select Year</option>
+          {exam!=null && data[exam].map((y)=>{
+            return(
+              <option value={y} key={y}>{y}</option>
+            )
+          })}
         </select>
         <p style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
           <select value={idtype} onChange={(e) => setidtype(e.target.value)} style={{ width: 100, padding: '10px', margin: '10px', marginRight: -1, borderRadius: '5px' }}>
