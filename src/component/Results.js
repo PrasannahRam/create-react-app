3ff88d3430b91166801a35f82c03959a51009cbe
PrasannahRam <rprasu2005@gmail.com>
2025-08-03 22:40:40 +0530

    completed result.js

--- src/component/Results.js
+++ src/component/Results.js
@@ -1,33 +1,43 @@
 import React from 'react'
 import './Result.css'
-function Results({result}) {
-    const titles = ['nic','indexno','exam','year']
-    const subjects = ['science','maths','english']
-   
-  return (
-    <div className='results_body'>
-        {console.log(result)
-        }
-        
-        {titles.map((title)=>{
-            return(
-            <p key={title}>
-                {console.log(result[title])}
-                
-                {title} ={result[title]}
-            </p>
-            )
+function Results({ result }) {
+    const titles = ['Nic', 'Indexno', 'Exam', 'Year']
+    const subjects = ['Science', 'Maths', 'English']
+
+    return (
+        <div className='results_body'>
+            {console.log(result)
+            }
+
+            {titles.map((title) => {
+                return (
+                    <p key={title} className='result_contents'>
+                        {title}     :{result[title]}
+                    </p>
+                )
+
+            })}
+            <table className="result-table">
+                    <tbody>
+                        {subjects.map((subject) => (
+                            <tr key={subject}>
+                                <td>
+                                    <p className="subject-name">{subject}</p>
+                                </td>
+                                <td className="subject-mark">
+                                    {result.result[subject]}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
             
-        })}
-        {subjects.map((subject)=>{
-            return(
-                <p>{subject} : {result.result[subject]}</p>
-            )
-        })}
-        
-     
-    </div>
-  )
+
+
+
+        </div>
+    )
 }
 
 export default Results
